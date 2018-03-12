19bf8870fe1abe229660c5de2c581caa897160ec
Jay Paulynice <julesbond007@users.noreply.github.com>
2018-03-12 00:36:15 -0400

    Update SpringConfig.java

--- src/main/java/com/nio/config/SpringConfig.java
+++ src/main/java/com/nio/config/SpringConfig.java
@@ -13,14 +13,8 @@ import org.springframework.context.annotation.FilterType;
  */
 @Configuration
 @ComponentScan(
-        basePackages = { SpringConfig.BASE_PACKGE },
+        basePackages = { "com.nio.http" },
         excludeFilters = @Filter(
                 type = FilterType.REGEX,
-                pattern = SpringConfig.EXCL_PACKAGE))
-public class SpringConfig {
-    /** base packages to start scanning beans */
-    protected static final String BASE_PACKGE = "com.nio.http";
-
-    /** we want to exclude the config classes from being picked up by spring */
-    protected static final String EXCL_PACKAGE = "com.nio.config.*";
-}
+                pattern = "com.nio.config.*"))
+public class SpringConfig {}
